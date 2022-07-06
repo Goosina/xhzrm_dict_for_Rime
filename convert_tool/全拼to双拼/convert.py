5605b43065adc3701bfab5355da46f9b74c8c5f1
Goosina <zhengxinyi0408@outlook.com>
2022-07-06 19:40:56 +0800

    upload

---
+++ convert_tool/全拼to双拼/convert.py
@@ -0,0 +1,76 @@
+import os
+
+string = "没有	mei you	44397392"
+
+shengmu_list1 = ['zh','ch','sh']
+shengmu_list2 = ['b','p','m','f','d','t','n','l','g','k','h','j','q','x','r','s','c','z','y','w']
+
+initials        = "initials.txt"
+finals1         = "finals1.txt"
+finals2         = "finals2.txt"
+
+def create_dict(textfile):
+    dict = {}
+    with open(textfile, "r", encoding="utf-8") as file:
+
+        for line in file:
+            (key, value) = line.split()
+            dict[key] = value
+    return dict
+
+#判断有无声母
+def has_shengmu(word):
+    s1=word[0]
+    for letter in shengmu_list2:
+        if s1 == letter:
+            if word[0:2] in shengmu_list1:
+                return 2
+            else:
+                return 1
+    return 0
+
+def output_shengmu_and_yunmu(word):
+    return([word[0:has_shengmu(word)],word[has_shengmu(word):]])
+
+def convert(string):
+    if '\t' in string:
+        ci,pinyin_whole,freq = string.split('\t')
+        pinyin_whole_new=''
+        for pinyin in pinyin_whole.split(' '):
+            shengmu,yunmu = output_shengmu_and_yunmu(pinyin)
+            try:
+                if shengmu == '':
+                    yunmu = finals2_dict[yunmu]
+                else:
+                    #print(yunmu)
+                    shengmu = initials_dict[shengmu]
+                    yunmu   = finals1_dict[yunmu]
+                pinyin_whole_new += shengmu+yunmu+' '
+            except:
+                return None
+        return ci+'\t'+pinyin_whole_new.rstrip ()+'\t'+freq
+    else:
+        return string
+
+initials_dict   = create_dict(initials) #声母
+finals1_dict    = create_dict(finals1)  #韵母
+finals2_dict    = create_dict(finals2)  #无声母
+
+def read_and_write(target,result):#yaml文件
+    with open(target,'r',encoding='utf-8') as f:
+        ret = f.readline()
+        while ret:
+            with open(result,'a',encoding='utf-8') as fr:
+                try:
+                    fr.write(convert(ret))
+                except:
+                    pass
+            ret = f.readline()
+        
+def convert_files():
+    file_list = os.listdir('./ku')
+    for file in file_list:
+        read_and_write('./ku/'+file,'./ku-convert/'+file.split('.dict')[0]+'.dict.yaml')
+
+convert_files()
+
---
+++ convert_tool/全拼to双拼/readme.md
@@ -0,0 +1,9 @@
+# 简介
+
+本脚本借鉴了https://github.com/jacobtung/flypy_dictionary
+
+finals1.txt、finals2.txt、initials.txt 三个文件是声母和韵母的映射关系。可以把全拼词库转化成任意双拼方案的码表。默认是小鹤的。
+
+将需要转换的词库放在ku文件夹，运行convert.py即可。
+
+词库必须为rime的词库。
